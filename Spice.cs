57a57b0bdfa290b497f33bb41fd34a081a492387
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-04-29 17:51:03 +0200

    spicev1

---
+++ Spice.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Spice
+{
+    // Constructor that takes no arguments:
+    public Spice()
+    {
+        Materials = "unknown";
+    }
+
+    // Constructor that takes one argument:
+    public Spice(string materials)
+    {
+        Materials = materials;
+    }
+
+    // Auto-implemented readonly property:
+    public string Materials { get; }
+
+    // Method that overrides the base class (System.Object) implementation.
+    public override string ToString()
+    {
+        return Materials;
+    }
+}
