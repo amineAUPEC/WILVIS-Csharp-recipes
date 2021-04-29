70f6f5a38d1cf9e0661f7bbbb9b5dac2248a395a
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-04-29 18:55:19 +0200

    1855

--- main.cs
+++ main.cs
@@ -31,7 +31,48 @@ class MainClass {
         Console.WriteLine(recipes2.Materials);
         Console.WriteLine(recipes2.Dishname);
         Console.WriteLine(recipes2.Quantities);
+
+        Console.WriteLine("Ingredients is : "+ recipes2.Materials+ " dishname : "+ recipes2.Dishname+ " quantities : "+ recipes2.Quantities );
+
         // public Recipes(string materials, string dishname, int quantities)
+
+
+        Console.WriteLine("BinarySearcher Class :");
+
+        BinaryTree binaryTree = new BinaryTree();
+        
+        binaryTree.Add(1);
+        binaryTree.Add(2);
+        binaryTree.Add(7);
+        binaryTree.Add(3);
+        binaryTree.Add(10);
+        binaryTree.Add(5);
+        binaryTree.Add(8);
+        
+        Node node = binaryTree.Find(5);
+        int depth = binaryTree.GetTreeDepth();
+        
+        Console.WriteLine("PreOrder Traversal:");
+        binaryTree.TraversePreOrder(binaryTree.Root);
+        Console.WriteLine();
+        
+        Console.WriteLine("InOrder Traversal:");
+        binaryTree.TraverseInOrder(binaryTree.Root);
+        Console.WriteLine();
+        
+        Console.WriteLine("PostOrder Traversal:");
+        binaryTree.TraversePostOrder(binaryTree.Root);
+        Console.WriteLine();
+        
+        binaryTree.Remove(7);
+        binaryTree.Remove(8);
+        
+        Console.WriteLine("PreOrder Traversal After Removing Operation:");
+        binaryTree.TraversePreOrder(binaryTree.Root);
+        Console.WriteLine();
+
+
+
 
 
 
